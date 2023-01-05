7913ee69dcfbb07567d46ea287ccd1b8054c7ee5
FiliaSologia <115911698+FiliaEunikeSologia@users.noreply.github.com>
2023-01-05 17:38:30 +0700

    Update Detail-Filia.php

--- MODUL3 FILIA FIX/pages/Detail-Filia.php
+++ MODUL3 FILIA FIX/pages/Detail-Filia.php
@@ -27,8 +27,8 @@
 <nav class="navbar navbar-expand-lg bg-primary " >
       <div class=" d-flex justify-content-left container-fluid">
             <ul class="navbar-nav">
-                  <li class="nav-item" ><a class="nav-link " href="Home-Mima.php" style="color:white;" >Home</a></li>
-                  <li class="nav-item"><a class="nav-link" href="ListCar-Mima.php" style="color:white;" >MyBookStore</a></li>
+                  <li class="nav-item" ><a class="nav-link " href="Home-Filia.php" style="color:white;" >Home</a></li>
+                  <li class="nav-item"><a class="nav-link" href="ListBuku-Filia.php" style="color:white;" >MyBookStore</a></li>
             </ul>
       </div>
 </nav>
